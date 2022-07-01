17c24a19ceaf46a7ede9a5af665fb1d21a7ec79c
+ <+>
2022-07-01 15:43:59 -0600

    commit tree cea0cd052979b7bce93c00bf1371f12a65fd49f9

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', () =>{
+     let x = document.querySelector('#text')
+    x.textContent = 'This is really cool!'
+    return ;
+
+    })
+ 
